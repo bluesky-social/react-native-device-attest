6b95bb7b58d4adc387c5b666b1d32378c687e9fd
Hailey <me@haileyok.com>
2025-06-24 01:06:23 -0700

    Initial commit
    Generated by create-expo-module 0.9.6.

---
+++ src/ReactNativeDeviceAttestView.tsx
@@ -0,0 +1,11 @@
+import { requireNativeView } from 'expo';
+import * as React from 'react';
+
+import { ReactNativeDeviceAttestViewProps } from './ReactNativeDeviceAttest.types';
+
+const NativeView: React.ComponentType<ReactNativeDeviceAttestViewProps> =
+  requireNativeView('ReactNativeDeviceAttest');
+
+export default function ReactNativeDeviceAttestView(props: ReactNativeDeviceAttestViewProps) {
+  return <NativeView {...props} />;
+}
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+// Reexport the native module. On web, it will be resolved to ReactNativeDeviceAttestModule.web.ts
+// and on native platforms to ReactNativeDeviceAttestModule.ts
+export { default } from './ReactNativeDeviceAttestModule';
+export { default as ReactNativeDeviceAttestView } from './ReactNativeDeviceAttestView';
+export * from  './ReactNativeDeviceAttest.types';
